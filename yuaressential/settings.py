40b25d5c3a4a1faa5f22459155c0fc27a86f2650
KUSH23 <kushalsai2007@gmail.com>
2023-02-04 02:41:15 +0530

    deploy

--- yuaressential/settings.py
+++ yuaressential/settings.py
@@ -53,7 +53,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_session_timeout.middleware.SessionTimeoutMiddleware',
 ]
+
+SESSION_EXPIRE_SECONDS = 1800 
+SESSION_EXPIRE_AFTER_LAST_ACTIVITY = True
+SESSION_TIMEOUT_REDIRECT = 'accounts/login'
 
 ROOT_URLCONF = 'yuaressential.urls'
 
