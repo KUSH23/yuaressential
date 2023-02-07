2856d0ab4862ca9373dfbe01624ea453591fa00d
KUSH23 <kushalsai2007@gmail.com>
2023-02-07 12:23:47 +0530

    debug error

--- yuaressential/settings.py
+++ yuaressential/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 #     'yuaressential/static',
 # ]
 
-AWS S3 Static Files Configuration
+#AWS S3 Static Files Configuration
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
