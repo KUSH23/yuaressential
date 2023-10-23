ae29e073db571dd59ae1dec412863ae796182b1c
KUSH23 <kushalsai2007@gmail.com>
2023-10-23 16:07:41 +0530

    new keys

--- yuaressential/deployment.py
+++ yuaressential/deployment.py
@@ -163,7 +163,7 @@ USE_TZ = True
 # AWS_LOCATION = 'static'
 
 
-STATIC_URL = '/static/'
+STATIC_URL = 'https://'+os.environ['WEBSITE_HOSTNAME']+'/static/'
 
 # STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
