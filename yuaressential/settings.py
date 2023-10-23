ec5babefc8651894b655f3d105b1af7f66c18f8b
KUSH23 <kushalsai2007@gmail.com>
2023-10-23 13:15:16 +0530

    production file added

--- yuaressential/settings.py
+++ yuaressential/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = "django-insecure-mm!3aq-*d*@upqlnmb0ysja7&^&v9%z#ikqq_mqa34#0zdns^+"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG=config('DEBUG', default=False, cast=bool)
+DEBUG=True
 
 ALLOWED_HOSTS = ['*']
 
